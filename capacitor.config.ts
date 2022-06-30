c70bac69e33341dfe4ff4199668d5073b674e895
Likhith Aalla <likhith@Likhiths-Air.lan>
2022-06-30 11:14:50 -0500

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.likhith.deploy',
+  appName: 'ngcap',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
